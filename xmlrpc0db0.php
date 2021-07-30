2a40979ba1799722a7620285b55e58ad4f298035
Banashree Gogoi <banashreegogoi19@gmail.com>
2021-07-30 15:31:30 +0530

    1st commit

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://startups4corona.com</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://startups4corona.com/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://startups4corona.com/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://startups4corona.com/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://startups4corona.com/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://startups4corona.com/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
